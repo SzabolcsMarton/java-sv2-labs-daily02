566aa0f515b23ef1c06f10ea31e7e61097879dda
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-18 15:57:26 +0200

    deleted accidently added setters

--- src/main/java/day01/Human.java
+++ src/main/java/day01/Human.java
@@ -13,15 +13,8 @@ public class Human {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
