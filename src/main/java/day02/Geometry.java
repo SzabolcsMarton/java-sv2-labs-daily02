c457f87f71734f93947120f7a91748b1be2b58a4
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-19 16:57:23 +0200

    refactored main() , add encoding to pom.xml

--- src/main/java/day02/Geometry.java
+++ src/main/java/day02/Geometry.java
@@ -4,6 +4,6 @@ public class Geometry {
 
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(5.00, 6.00);
-        System.out.println(rectangle.Calculate());
+        System.out.println(rectangle.calculateArea());
     }
 }
