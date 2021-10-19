97cf9a43d37eb989cf2ed93bb96a339c7a5dd72a
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-19 15:41:52 +0200

    created the 2 class and add the methods and attrbs.

---
+++ src/main/java/day02/Geometry.java
@@ -0,0 +1,9 @@
+package day02;
+
+public class Geometry {
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(5.00, 6.00);
+        System.out.println(rectangle.Calculate());
+    }
+}
