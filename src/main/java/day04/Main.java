77e7019db5dc7ade0b67af360e01c51ba8177fe2
SzabolcsMarton <maszabolcs@gmail.com>
2021-10-21 16:43:28 +0200

    update main()

--- src/main/java/day04/Main.java
+++ src/main/java/day04/Main.java
@@ -3,10 +3,10 @@ package day04;
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
-        GasStation gasStation = new GasStation(467);
-        System.out.println("A tankolas ara: " + gasStation.tankCar(car,30));
+        GasStation gasStation = new GasStation(468);
+        System.out.println("A tankolas ara: " + gasStation.tankCar(car,40));
 
-        car.setKm(452);
+        car.setKm(635);
 
         System.out.println("A fogyasztas: " + car.calculateConsumption());
     }
